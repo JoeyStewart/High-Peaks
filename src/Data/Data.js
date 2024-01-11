6db89b669dcc2598dfa8cd2207975b47cf0fc245
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-11 15:34:11 -0500

    Add state object used for search function

---
+++ src/Data/Data.js
@@ -0,0 +1,3 @@
+const stateData = [{ id: 1, state: 'New York', snippet: 'Mt. Marcy' },
+{ id: 2, state: 'New Jersey', snippet: 'High Point' },
+{ id: 3, state: 'Vermont', snippet: 'Mt. Mansfield'}] 
