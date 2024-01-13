df5806088184121c0df090954c1ac97b5af03072
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-12 21:35:35 -0500

    created exception for null

--- src/Moutain/Mountain.js
+++ src/Moutain/Mountain.js
@@ -1,26 +1,22 @@
 import Card from '../Card/Card.js';
 import './Mountain.css';
 
-
-function Mountain({mountains}){
-
-
-const mountainCards = mountains.map(mountain => {
-   return (
-       <Card
-       state={mountain.state}
-       snippet={mountain.snippet}
-       id={mountain.id}
-       key={mountain.id}
-       />
-   )
-})
- return (
-   <div className='mountains-container'>
-     {mountainCards}
-   </div>
- )
+function Mountain({ mountain }) {
+    console.log(mountain)
+    if (!mountain) {
+      return null;
+    }
+  return (
+    <div className='mountains-container'>
+      <Card
+        state={mountain.state}
+        mountain={mountain.mountain}
+        snippet={mountain.snippet}
+        id={mountain.id}
+        // key={mountain.id}
+      />
+    </div>
+  );
 }
-
 
 export default Mountain;
