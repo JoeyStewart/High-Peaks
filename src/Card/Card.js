ccd7c097451d6097163452d392c450d012074e7d
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-13 13:44:02 -0500

    add save

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,13 +2,24 @@ import './Card.css';
 
 //Button will be used for favorites.
 //Change props to snippit and ID when mock data is finished.
-function Card({state, snippet, mountain}){
+function Card({state, snippet, mountain, saveArticle}){
+    
+    const handleSave = () => {
+        const article = {
+          state,
+          mountain,
+          snippet,
+          id,
+        };
+        saveArticle(article);
+      };
+
  return (
    <div className='card'>
      <h2>{state}</h2>
      <h3>{mountain}</h3>
      <p>{snippet}</p>
-     <button>😩</button> 
+     <button onClick={handleSave}>Save</button>
    </div>
  )
 }
