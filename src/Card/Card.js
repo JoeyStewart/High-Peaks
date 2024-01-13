c8080729f93f00ffd36851262a776ba2ca26d592
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-13 18:56:24 -0500

    Changed the card for save function

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 //Button will be used for favorites.
 //Change props to snippit and ID when mock data is finished.
 function Card({state, snippet, mountain, id, saveArticle}){
-    
+    console.log(saveArticle)
     const handleSave = () => {
         const article = {
           state,
