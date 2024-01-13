9f262ab9530d1624b8443273f3b2ad5d1a1baa72
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-13 18:57:15 -0500

    Save function and file created

--- src/Saved/SavedMountains.js
+++ src/Saved/SavedMountains.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Card from '../Card/Card';
 
-function savedArticles({ savedArticles }) {
-    return (
-      <div>
-        <h1>Favorite Mountains</h1>
-        {savedArticles.map((article) => (
-          <Card
-            key={article.id}
-            state={article.state}
-            mountain={article.mountain}
-            snippet={article.snippet}
-          />
-        ))}
-      </div>
-    );
-  }
+function SavedArticles({ savedMountains }) {
+  console.log(savedMountains)
+  return (
+    <div>
+      <h1>Favorite Mountains</h1>
+      {savedMountains.map((article) => (
+        <Card
+          key={article.id}
+          state={article.state}
+          mountain={article.mountain}
+          snippet={article.snippet}
+        />
+      ))}
+    </div>
+  );
+}
 
-export default savedArticles
+export default SavedArticles;
