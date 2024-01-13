c65726c58e845fab9e39eb16a9fd99958b922aad
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-12 21:35:08 -0500

    Deleted key

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,10 +2,11 @@ import './Card.css';
 
 //Button will be used for favorites.
 //Change props to snippit and ID when mock data is finished.
-function Card({state, snippet, id}){
+function Card({state, snippet, mountain}){
  return (
    <div className='card'>
-     <h3>{state}</h3>
+     <h2>{state}</h2>
+     <h3>{mountain}</h3>
      <p>{snippet}</p>
      <button>😩</button> 
    </div>
