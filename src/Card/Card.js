d3bd853840585599db1d44df4afb64f1e2f5859c
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-13 23:26:55 -0500

    Add propTypes and change to handleSave

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,35 +1,25 @@
 import './Card.css';
 import PropTypes from 'prop-types';
-//Button will be used for favorites.
-//Change props to snippit and ID when mock data is finished.
-function Card({state, snippet, mountain, id, saveArticle}){
-    console.log(saveArticle)
-    const handleSave = () => {
-        const article = {
-          state,
-          mountain,
-          snippet,
-          id,
-        };
-        saveArticle(article);
-      };
 
- return (
-   <div className='card'>
-     <h2>{state}</h2>
-     <h3>{mountain}</h3>
-     <p>{snippet}</p>
-     <button onClick={handleSave}>Save</button>
-   </div>
- )
+function Card({ state, snippet, mountain, handleSave, id }) {
+  console.log('Props received in Card:', { state, snippet, mountain, handleSave, id });
+console.log(handleSave)
+  return (
+    <div className='card'>
+      <button onClick={()=>handleSave(state, mountain, snippet, id)}>Save</button>
+      <h2>{state}</h2>
+      <h3>{mountain}</h3>
+      <p>{snippet}</p>
+    </div>
+  );
 }
 
 Card.propTypes = {
   state: PropTypes.string.isRequired,
-  snippet: PropTypes.string.isRequired,
+  snippet: PropTypes.element.isRequired,
   mountain: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
-  saveArticle: PropTypes.func.isRequired,
+  handleSave: PropTypes.func.isRequired,
 };
 
 export default Card;
