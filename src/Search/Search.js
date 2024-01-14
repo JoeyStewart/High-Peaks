186dcd17483bf86d54c05cc3e7cf32def9bb5553
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-13 19:42:52 -0500

    Add propTypes

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import './Search.css';
 
 function Search({ searchState }) {
@@ -37,5 +38,9 @@ function Search({ searchState }) {
     </form>
   );
 }
+
+Search.propTypes = {
+  searchState: PropTypes.func.isRequired,
+};
 
 export default Search;
