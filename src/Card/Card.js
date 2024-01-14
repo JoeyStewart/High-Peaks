15a33c16645b222d3521b2002d18e099cafef14f
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-13 19:42:10 -0500

    Add propTypes

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,5 +1,5 @@
 import './Card.css';
-
+import PropTypes from 'prop-types';
 //Button will be used for favorites.
 //Change props to snippit and ID when mock data is finished.
 function Card({state, snippet, mountain, id, saveArticle}){
@@ -24,5 +24,12 @@ function Card({state, snippet, mountain, id, saveArticle}){
  )
 }
 
+Card.propTypes = {
+  state: PropTypes.string.isRequired,
+  snippet: PropTypes.string.isRequired,
+  mountain: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  saveArticle: PropTypes.func.isRequired,
+};
 
 export default Card;
